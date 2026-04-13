630237cb9ef5642f1287e16d3892f4ead500c412
ElliottStevanus <EWS38@pitt.edu>
2026-04-13 18:48:23 -0400

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -4,14 +4,12 @@ export function runXSLT(path, xmlDoc, container) {
         .then(r => r.text())
         .then(xsltText => {
 
-            const xsltDoc =
-                new DOMParser().parseFromString(xsltText, "text/xml");
+            const xsltDoc = new DOMParser().parseFromString(xsltText, "text/xml");
 
             const processor = new XSLTProcessor();
             processor.importStylesheet(xsltDoc);
 
-            const result =
-                processor.transformToFragment(xmlDoc, document);
+            const result = processor.transformToFragment(xmlDoc, document);
 
             container.innerHTML = "";
             container.appendChild(result);
