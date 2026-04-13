3f7ba7db0b779ab798a8819b0ce9038421d08671
ElliottStevanus <EWS38@pitt.edu>
2026-04-13 17:33:18 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,4 +30,23 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     document.getElementById("view-analysis").onclick = () =>
         runXSLT("xsl/analysis.xsl", annotatedDoc, container);
+    function runXSLT(path, xmlDoc, container) {
+
+    fetch(path)
+        .then(r => r.text())
+        .then(xsltText => {
+
+            const xsltDoc =
+                new DOMParser().parseFromString(xsltText, "text/xml");
+
+            const processor = new XSLTProcessor();
+            processor.importStylesheet(xsltDoc);
+
+            const result =
+                processor.transformToFragment(xmlDoc, document);
+
+            container.innerHTML = "";
+            container.appendChild(result);
+        });
+}
 });
